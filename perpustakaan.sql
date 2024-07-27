ff2a0f2061a8bd32edd2fd8993a621b7d8fc5890
Wannn <108000407+h3h3y@users.noreply.github.com>
2024-07-27 09:50:33 +0700

    Add files via upload

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 25 Jun 2024 pada 06.30
--- Versi server: 10.4.28-MariaDB
--- Versi PHP: 8.2.4
+-- Host: localhost:3306
+-- Waktu pembuatan: 27 Jul 2024 pada 02.49
+-- Versi server: 5.7.33
+-- Versi PHP: 8.1.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,15 +32,34 @@ CREATE TABLE `buku` (
   `judul` varchar(200) NOT NULL,
   `penulis` varchar(200) NOT NULL,
   `tanggal_terbit` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `buku`
 --
 
 INSERT INTO `buku` (`id`, `judul`, `penulis`, `tanggal_terbit`) VALUES
-(1, 'Wleowleowleo', 'dikadarksistem', '2024-06-04'),
-(2, 'gktw', 'Dhikanjut', '2024-06-04');
+(2, 'Belajar Php 1', 'Sandika Galih', '2024-06-04'),
+(4, 'tes', 'tes', '2024-07-16');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(20) NOT NULL,
+  `nama_mahasiswa` varchar(100) NOT NULL,
+  `no_telepon` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama_mahasiswa`, `no_telepon`) VALUES
+('22.240.0080', 'iwan', '123');
 
 --
 -- Indexes for dumped tables
@@ -53,6 +72,12 @@ ALTER TABLE `buku`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -60,7 +85,7 @@ ALTER TABLE `buku`
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
